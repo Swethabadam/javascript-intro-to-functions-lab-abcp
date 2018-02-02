043674d273ba8a09e80e3924a26e01512d7b0765
SWETHA BADAM <sweth.badam@gmail.com>
2018-02-02 17:35:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+
+}
+
+function sayHiToGrandma(string){
+  var str;
+ if(string===string.toLowerCase){
+   str=console.log("I can't hear you!");
+   return str;
+ } 
+}
+ /*else if(string.toUpperCase===string){
+ var string=console.log("YES INDEED!");
+ return string
+}
+else if(string==="I love you, Grandma."){
+  var string=console.log("I love you, too.");
+  return string
+}*/
+
